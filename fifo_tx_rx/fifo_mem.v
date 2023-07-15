c5c207c07a097a9ff2c459bcc4eccace16ea195d
Lê Ngọc Minh Thư <116614977+lnmthu1505@users.noreply.github.com>
2023-07-15 16:43:52 +0700

    change to combinational logic to match latency

--- fifo_tx_rx/fifo_mem.v
+++ fifo_tx_rx/fifo_mem.v
@@ -11,31 +11,43 @@ module fifo_mem #(
    input  wire [ASIZE-1:0] rd_addr   , // Read address
    input  wire [ASIZE-1:0] wr_addr   , // write address 
    input  wire [WIDTH-1:0] data_in   , // Data 8-bit Input
-   output reg  [WIDTH-1:0] data_out   //  Data 8-bit Output
+   output wire [WIDTH-1:0] data_out   //  Data 8-bit Output
 ); //cadence black_box  
 
    parameter DEPTH = 1 << ASIZE; 
-   reg [WIDTH-1 : 0] mem [0:DEPTH-1]; 
-   
+   reg [WIDTH-1 : 0] mem [0:DEPTH-1];
+   reg [WIDTH-1:0] tmp_data_out;
+   //reg tmp_wrt_en;
+   assign data_out = rd_en ? tmp_data_out : mem[rd_addr];//data_out;
+
    always @(posedge rd_clk or negedge rst_n) begin
       if (!rst_n) begin
-            data_out <= 16'h0; 
+         tmp_data_out <= 8'h00; 
       end else begin
-         if (rd_en) begin
-            data_out <= mem[rd_addr];   
-         end
+         //if (rd_en) begin
+            tmp_data_out <= mem[rd_addr];
+         //end else begin
+         //   data_out <= data_out;
+         //end
       end
    end 
-   
+   /*always@(posedge wrt_clk or negedge rst_n) begin
+      if (!rst_n) begin
+         tmp_wrt_en <= 1'b0;
+      end else begin
+         tmp_wrt_en <= wrt_en;
+      end
+   end*/
    always @(posedge wrt_clk or negedge rst_n) begin
       if (!rst_n) begin
-            mem[wr_addr] <= 16'h0; 
+            mem[wr_addr] <= 8'h00; 
       end else begin
          if (wrt_en) begin
             mem[wr_addr] <= data_in;   
+         end else begin
+            mem[wr_addr] <= mem[wr_addr];
          end
       end
    end
 
 endmodule
-
